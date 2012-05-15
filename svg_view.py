ea57d721a551e6fed20399c156e32fb178e650f5
Jakob Truelsen <jakob@scalgo.com>
2012-05-15 11:34:46 +0200

    Add support for several open documents, using tabs

--- svg_view.py
+++ svg_view.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,8 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from PyQt4 import QtSvg,QtCore,QtGui,Qt
-import sys
-import signal
+import sys, signal, os
 
 class SvgWidget(QtSvg.QSvgWidget):
     location_changed = QtCore.pyqtSignal(QtCore.QPointF)
@@ -34,17 +33,12 @@ class SvgWidget(QtSvg.QSvgWidget):
         self.center_y = self.defViewBox.center().y()
         self.updateViewBox(self.size())
         self.repaint()
-
-    def load(self, path):
-        QtSvg.QSvgWidget.load(self, path)
-        self.defViewBox = self.renderer().viewBoxF()
-        self.center()
         
     def resizeEvent(self, evt):
         self.updateViewBox( evt.size())
         QtSvg.QSvgWidget.resizeEvent(self, evt)
         
-    def __init__(self):
+    def __init__(self, path):
         QtSvg.QSvgWidget.__init__(self)
         self.setMouseTracking(True)
         self.ds = None
@@ -53,6 +47,9 @@ class SvgWidget(QtSvg.QSvgWidget):
         self.center_y = 0
         self.setPalette( QtGui.QPalette( QtCore.Qt.white ) );
         self.setAutoFillBackground(True)
+        QtSvg.QSvgWidget.load(self, path)
+        self.defViewBox = self.renderer().viewBoxF()
+        self.center()
         
     def updateLocation(self, pos):
         self.location_changed.emit(QtCore.QPointF(
@@ -91,19 +88,42 @@ class MainWindow(QtGui.QMainWindow):
     def showLocation(self, point):
         self.statusbar.showMessage("%f %f"%(point.x(), point.y()))
 
+    def load(self, path):
+        svg = SvgWidget(path)
+        svg.location_changed.connect(self.showLocation)
+        self.tabs.setCurrentIndex( self.tabs.addTab(svg, os.path.basename("%s"%path)))
+        
+    def closeTab(self):
+        if not self.tabs.currentWidget(): return
+        self.tabs.removeTab(self.tabs.currentIndex())
+
+    def center(self):
+        if not self.tabs.currentWidget(): return
+        self.tabs.currentWidget().center()
+
+    def nextTab(self):
+        if not self.tabs.currentWidget(): return
+        self.tabs.setCurrentIndex( (self.tabs.currentIndex() + 1)%self.tabs.count());
+ 
+    def prevTab(self):
+        if not self.tabs.currentWidget(): return
+        self.tabs.setCurrentIndex( (self.tabs.currentIndex() - 1)%self.tabs.count());
+
     def open(self):
         path = QtGui.QFileDialog.getOpenFileName(
             self, "Open File", filter=tr("Svg documents (*.svg);;Any files (*.*)"))
-        if path: self.svg.load(path)
+        if path: self.load(path)
 
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
-        self.svg = SvgWidget()
-        self.setCentralWidget(self.svg)
+        self.tabs = QtGui.QTabWidget(self)
+        self.tabs.setMovable(True)
+        self.tabs.setTabsClosable(True)
+        self.tabs.tabCloseRequested.connect(self.closeTab)
+        self.setCentralWidget(self.tabs)
         self.resize(800, 600)
         self.statusbar = QtGui.QStatusBar(self)
         self.setStatusBar(self.statusbar)
-        self.svg.location_changed.connect(self.showLocation)
         
         self.menubar = QtGui.QMenuBar(self)
         self.menuFile = QtGui.QMenu(self.menubar)
@@ -114,25 +134,41 @@ class MainWindow(QtGui.QMainWindow):
         self.actionOpen.setShortcuts(QtGui.QKeySequence.Open);
         self.actionQuit = QtGui.QAction(self)
         self.actionQuit.setShortcuts(QtGui.QKeySequence.Quit);
+        self.actionClose = QtGui.QAction(self)
+        self.actionClose.setShortcuts(QtGui.QKeySequence.Close)
         self.actionCenter = QtGui.QAction(self)
         self.actionCenter.setShortcuts(QtGui.QKeySequence(tr("Space")));
+        self.actionNext = QtGui.QAction(self)
+        self.actionNext.setShortcuts(QtGui.QKeySequence(tr("Page Down")));
+        self.actionPrev = QtGui.QAction(self)
+        self.actionPrev.setShortcuts(QtGui.QKeySequence(tr("Page Up")));
+ 
         self.menuFile.addAction(self.actionOpen)
         self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionClose)
         self.menuFile.addAction(self.actionQuit)
         self.menuEdit.addAction(self.actionCenter)
+        self.menuEdit.addAction(self.actionNext)
+        self.menuEdit.addAction(self.actionPrev)
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuEdit.menuAction())
 
-        self.actionCenter.triggered.connect(self.svg.center)
+        self.actionCenter.triggered.connect(self.center)
+        self.actionNext.triggered.connect(self.nextTab)
+        self.actionPrev.triggered.connect(self.prevTab)
         self.actionQuit.triggered.connect(self.close)
         self.actionOpen.triggered.connect(self.open)
+        self.actionClose.triggered.connect(self.closeTab)
 
         self.setWindowTitle(tr("Svg Viewer"))
         self.menuFile.setTitle(tr("&File"))
         self.menuEdit.setTitle(tr("&Edit"))
         self.actionOpen.setText(tr("&Open"))
+        self.actionClose.setText(tr("&Close Tab"))
         self.actionQuit.setText(tr("&Quit"))
         self.actionCenter.setText(tr("&Center"))
+        self.actionNext.setText(tr("&Next Tab"))
+        self.actionPrev.setText(tr("&Prev Tab"))
         
 def handleIntSignal(signum, frame):
     QtGui.qApp.closeAllWindows()
@@ -140,16 +176,15 @@ def handleIntSignal(signum, frame):
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     
-    if len(app.arguments()) > 2 or '-h' in app.arguments()[1:] or '--help' in app.arguments()[1:]:
+    if  '-h' in app.arguments()[1:] or '--help' in app.arguments()[1:]:
         print "Usage: svg_view.py <path_to_svg_file>?"
         exit
 
     window = MainWindow()
     window.show()
 
-    
-    if len(app.arguments()) > 1:
-        window.svg.load(app.arguments()[1])
+    for path in app.arguments()[1:]:
+        window.load(path);
 
     #This is a hack to let the interperter run once every 1/2 second to catch sigint
     timer = QtCore.QTimer()
